207b44c6ec76f04b551242748b93eed8030f5567
daanownd <daanvanderthiel@gmail.com>
2016-01-07 17:15:44 +0100

    code

--- code/testhtmlscraper.py
+++ code/testhtmlscraper.py
@@ -1,4 +1,4 @@
-
+import io
 import os
 import sys
 import csv
@@ -17,7 +17,7 @@ with open("alleUrls.csv", "r") as urls:
         dom = DOM(url.download(cached = True))
         # geeft de week
         i = 1
-        with open("allmusic.csv", "wb") as f:
+        with io.open("allmusic.csv", "w",charset = "utf8") as f:
             for week in dom.by_class("weeknr")[1:2]:
                 print week.content
             # de lijst van de top 40 selecteren
@@ -32,24 +32,25 @@ with open("alleUrls.csv", "r") as urls:
                     i = i+1 # opletten met resetten
                     # de artiest selecteren
                     for artiest in e.by_class("credit"):
-                        muziekGegevens += artiest.content
+                        muziekGegevens += artiest.content + ","
                     #positie
                     for inner in e.by_tag("strong")[1:2]:
                         print inner.content , "1:2"
-                        muziekGegevens += inner.content
+                        muziekGegevens += inner.content + ","
                     # hoogste notering
                     for inner in e.by_tag("strong")[2:3]:
                         print inner.content , "2:3"
+                        muziekGegevens += inner.content + ","
                     # aantal punten
                     for inner in e.by_tag("strong")[3:4]:
                         print inner.content , "3:4"
-                        muziekGegevens += inner.content
+                        muziekGegevens += inner.content + ","
                     # jaar van het nummer
                     for inner in e.by_tag("strong")[4:5]:
                         print inner.content.strip() , "4:5"
-                        muziekGegevens += inner.content
+                        muziekGegevens += inner.content + ","
                     h = HTMLParser.HTMLParser()
-                    h.unescape(muziekGegevens)
+                    muziekGegevens = h.unescape(muziekGegevens)
                     f.write(muziekGegevens)
 f.close
 urls.close
