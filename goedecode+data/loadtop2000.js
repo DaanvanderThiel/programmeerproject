a85631269958e2b05c600572156f140d65abe889
daanownd <daanvanderthiel@gmail.com>
2016-01-14 13:24:59 +0100

    code aanpassingen

--- goedecode+data/loadtop2000.js
+++ goedecode+data/loadtop2000.js
@@ -1,35 +1,20 @@
 window.onload = function() {
-  var dataDict = []
-  var oneYear = []
+  var DATADICT = []
   var allYear = []
   jaren = [1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
   d3.json("json2000.json", function(data) {
   var data = data.map(function(d) {
 
-      dataDict.push({
+      DATADICT.push({
         artiest: d[0],
         nummer: d[1],
         jaar: d[2],
         posities : [d[3],d[4],d[5],d[6],d[7],d[8],d[9],d[10],d[11],d[12],d[13],
         d[14],d[15],d[16],d[17],d[18],d[19]]
       });
-      //console.log(dataDict)
+      //console.log(DATADICT)
   });
-  l = dataDict[0]
-  console.log(dataDict[0])
-  console.log(l.j2009)
-  for (var i = 0; i < dataDict.length ; i++){
-    if (dataDict[i].posities[0] != "*" && dataDict[i].posities[0] != "-"){
-      oneYear.push({
-        jaartal: 1999,
-        nummer: dataDict[i].nummer,
-        positie:dataDict[i].posities[0]
-      })
 
-    }
-
-  }
-  console.log(oneYear)
   HEIGHT = 40100
   WIDTH = 1500
   WIDTHBLOCK = 50
@@ -53,27 +38,28 @@ window.onload = function() {
              return !isNaN(parseFloat(n)) && isFinite(n);
            }
 
-  maakKolom(dataDict)
-  //console.log(dataDict)
+  maakKolom(DATADICT)
+  //console.log(DATADICT)
   function maakKolom(data) {
     for (var p = 3; p <jaren.length+3; p++){
-        for (var i = 0; i < dataDict.length ; i++){
+        for (var i = 0; i < DATADICT.length ; i++){
 
-        if (isNumeric(dataDict[i].posities[p-3])){
+        if (isNumeric(DATADICT[i].posities[p-3])){
           allYear.push({
             jaartal: 1996+p,
-            nummer: dataDict[i].nummer,
-            positie:dataDict[i].posities[p-3]
+            nummer: DATADICT[i].nummer,
+            artiest: DATADICT[i].artiest,
+            positie:DATADICT[i].posities[p-3]
         })
       }
 
      }
    }
-   //console.log(allYear)
+   // zelfde nummer :(
        var rectangles = svg.selectAll('rect').data(allYear);
        rectangles.enter().append('rect')
-        .attr('x', function(d, i) { return (d.jaartal - 1998) * 65})
-        .attr('y', function(d, i)  { return (d.positie * 20) } )
+        .attr('x', function(d) { return (d.jaartal - 1998) * 65})
+        .attr('y', function(d)  { return (d.positie * 20) } )
         .attr('width', WIDTHBLOCK)
         .attr('height', HEIGHTBLOCK)
         .attr('fill', 'gray')
@@ -81,124 +67,79 @@ window.onload = function() {
         .text(function(d) { return d.nummer })
         .on('mouseover', function(d) {
           svg.append("text")
-            .attr("y", "3000")
+            .attr("y", "30")
             .attr("x", "300")
             .attr("class", "hover")
             .attr("font-family", "sans-serif")
             .attr("font-size", "48px")
+            .style("position", "absolute")
             .style("fill", "red")
-            .text (d.nummer)
-            lineData = []
-            for( var u= 0; u<dataDict.length; u++){
-              if (dataDict[u].nummer == d.nummer){
-            for (var i = 0; i < 17; i++) {
-              if (isNumeric(dataDict[u].posities[i])){
-              lineData.push({
-                x: ((i+ 1) * 65 + WIDTHBLOCK),
-                y: ((dataDict[u].posities[i]) * 20 + HEIGHTBLOCK)
-              })
-                }
-              if (isNumeric(dataDict[u].posities[i+1]) && i+1 < 17){
-              lineData.push({
-                x: ((i+1) * 65 + WIDTHBLOCK +15),
-                y:  ((dataDict[u].posities[i+1]) * 20 + HEIGHTBLOCK)
-              })
-            }
+            .text (d.artiest + ":" + d.nummer);
+            creatLine("path",d.nummer,d.artiest)
 
-            var line = d3.svg.line()
-                .x(function(d){return d.x;})
-                .y(function(d){return d.y;})
-                .interpolate("linear");
-            svg.append("path")
-                .attr("d", function(d) { return line(lineData)})
-                .attr("transform", "translate(0,0)")
-                .style("stroke-width", 2)
-                    .style("stroke", "black")
-                    .style("fill", "none");
-            }
-          }
-        }
-
-
-          //console.log(d);
         })
+        .on("click", function(d) {
+        creatLine("clickLine",d.nummer,d.artiest)
+         })
         .on("mouseout", function(d) {
-
-            svg.select("text.hover").remove();
-            svg.selectAll("path").remove()
+            svg.selectAll(".hover").remove();
+            svg.selectAll(".path").remove()
           })
-
-
-//       for( var u= 0; u<dataDict.length; u++){
-//         lineData = []
-//       for (var i = 0; i < 17; i++) {
-//         lineData.push({
-//           x: ((i+ 1) * 65 + WIDTHBLOCK),
-//           y: ((dataDict[u].posities[i]) * 20 + HEIGHTBLOCK)
-//         })
-//         lineData.push({
-//           x: ((i+1) * 65 + WIDTHBLOCK +15),
-//           y:  ((dataDict[u].posities[i+1]) * 20 + HEIGHTBLOCK)
-//         })
-//       }
-//       tekenLijnen(lineData,"black")
-//     }
-//
-//       function tekenLijnen(data,kleur){
-//         // begin of drawing lines
-//         var line = d3.svg.line()
-//             .x(function(d){return d.x;})
-//             .y(function(d){return d.y;})
-//             .interpolate("linear");
-//
-// // var line = d3.svg.line()
-// //     .x(function(d,i){console.log(d);k +=1; if (k == 17){
-// //       k = 0;} return(k+1) * 65})
-// //     .y(function(d,i){console.log(d.posities[k]);
-// //     if (isNumeric(d.posities[k])){ return 0;} ; return (d.posities[k] * 20) } )
-// //     .interpolate("linear");
-//
-// svg.append("path")
-//     .attr("d", function(d) { console.log(line(data));return line(data)})
-//     .attr("transform", "translate(0,0)")
-//     .style("stroke-width", 2)
-//         .style("stroke", kleur)
-//         .style("fill", "none");
-// //end of drawing lines
-//        }
-        /*
-       newRect = svg.append("rect")
-      .attr("x", 20 + 50)
-      .attr("y", 20 + i*50)
-      .attr("width", 15)
-      .attr("height", 15)
-
-      .attr("fill", "red")
-      .attr("id", "rectLabel")
-      .text(allYear[i].nummer)
-      .on("mouseover", function(d) {
-        svg.append("text")
-          .attr("y", "100")
-          .attr("x", "200")
-          .attr("class", "hover")
+          .on("dblclick", function(d) {
+            document.getElementById('audioElement').play()
+           })
+function creatLine(path,nummer,artiest){
+  lineData = []
+  for( var u= 0; u<DATADICT.length; u++){
+    if (DATADICT[u].nummer == nummer && DATADICT[u].artiest == artiest){
+  for (var i = 0; i < 17; i++) {
+    if (isNumeric(DATADICT[u].posities[i])){
+    lineData.push({
+      x: ((i+ 1) * 65 + WIDTHBLOCK),
+      y: ((DATADICT[u].posities[i]) * 20 + HEIGHTBLOCK)
+    })
+      }
+    if (isNumeric(DATADICT[u].posities[i+1]) && i+1 < 17){
+    lineData.push({
+      x: ((i+1) * 65 + WIDTHBLOCK +15),
+      y:  ((DATADICT[u].posities[i+1]) * 20 + HEIGHTBLOCK)
+    })
+  }
+  var line = d3.svg.line()
+      .x(function(d){return d.x;})
+      .y(function(d){return d.y;})
+      .interpolate("linear");
+  svg.append("path")
+      .attr("d", function(d) { return line(lineData)})
+      .attr("transform", "translate(0,0)")
+      .classed(path, true)
+      .style("stroke-width", 2)
+          .style("stroke", "black")
+          .style("fill", "none");
+  svg.append("text")
+          .attr("y", "30")
+          .attr("x", "30")
+          .classed(path, true)
           .attr("font-family", "sans-serif")
-          .attr("font-size", "12px")
-          .style("fill", "black")
-          .text(function(d) {
-            console.log(d);
-            return allYear[0].nummer;
-          })
-      })
-    .on("mouseout", function(d) {
-
-        svg.select("text.hover").remove();
-      })*/
+          .attr("font-size", "48px")
+          .style("fill", "red")
+          // aanpassen!!
+  }
+}
+}
+}
+function kleurBlock(nummer,artiest,positie){
+  for( var u= 0; u<DATADICT.length; u++){
+    if (DATADICT[u].nummer == nummer && DATADICT[u].artiest == artiest){
+        if
+      }
+  }
+}
 console.log(allYear , "allyear")
-console.log(dataDict , "dataDict")
+console.log(DATADICT , "DATADICT")
   }
 
 });
-
 
 
 }
