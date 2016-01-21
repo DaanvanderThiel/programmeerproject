03f520335dc81696ee70ad68c12dd95b9526c672
daanownd <daanvanderthiel@gmail.com>
2016-01-21 14:52:32 +0100

    lijnen

--- goedecode+data/loadtop2000.js
+++ goedecode+data/loadtop2000.js
@@ -1,11 +1,15 @@
+
 breedteblokje = 10
 hoogteblockje = 0.19
 afstandtussenblokje = 0.04
-console.log("search")
+counter = 0
+colors = ["wheat","sandyBrown","seaGreen","grey","steelBlue","YellowGreen","Teal","red","green","yellow","purple","orange","blue","Turquoise"]
+d3.hsl
 window.onload = function() {
-
+//https://github.com/twitter/typeahead.js
   var DATADICT = []
   var allYear = []
+  var nummers = []
   HEIGHT = 12040
   WIDTH = 400
   WIDTHBLOCK = 20
@@ -14,12 +18,12 @@ window.onload = function() {
   breedteblokje = 10
   hoogteblockje = 0.19
   afstandtussenblokje = 0.04
-  function completeAndRedirect() {
-    console.log("sljf")
-    return "france"
-}
+
   //console.log(document.getElementById("searchfield").innerHTML)
   d3.select("body").append("div").attr("class","slider").style("position","fixed").style("top", "100px").style("left", "400px")
+  //tabel
+  d3.select("body").append("ul").attr("class","tabel").style("position","fixed").style("top", "200px").style("left", "600px")
+
   var svgslider = d3.select(".slider")
                           .append("svg")
                           .attr("width", "600")
@@ -37,7 +41,7 @@ window.onload = function() {
                           .style("stroke-width", 1);
 
   jaren = [1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]
-  d3.select("body").append("h1").html("beweeg de muis over de rectangle voor de artiest en het nummer").attr("class","p2")
+  d3.select("body").append("h4").html("beweeg de muis over de rectangle voor de artiest en het nummer").attr("class","p2").style("padding-top","40px")
   d3.select(".p2").style("position","fixed").style("top", "30px").style("left", "400px")
   d3.json("json2000.json", function(data) {
   var data = data.map(function(d) {
@@ -49,6 +53,9 @@ window.onload = function() {
         posities : [d[3],d[4],d[5],d[6],d[7],d[8],d[9],d[10],d[11],d[12],d[13],
         d[14],d[15],d[16],d[17],d[18],d[19]]
       });
+      nummerArtiest = d[0]+ ":"+d[1]
+
+      nummers.push(nummerArtiest)
       //console.log(DATADICT)
   });
   var svg = d3.select("body")
@@ -114,7 +121,6 @@ window.onload = function() {
       }
      }
    }
-   // zelfde nummer :(
        var rectangles = svg.selectAll('rect').data(allYear);
        rectangles.enter().append('rect')
         .attr('x', function(d) { return (d.jaartal - 1998) * (WIDTHBLOCK+2)})
@@ -123,27 +129,22 @@ window.onload = function() {
         .attr('height', HEIGHTBLOCK)
         .attr('fill', function(d){return kleurBlock(d.nummer,d.artiest,d.positie,d.jaartal)})
         .attr('class', 'rectLabel')
-        //.text(function(d) { return d.nummer })
 
         .on('mouseover', function(d) {
           d3.select(".p2").html(d.artiest + ":" + d.nummer);
-          // svg.append("text")
-          //   //.attr("y", "30")
-          //   //.attr("x", "410")
-          //   .attr("class", "hover")
-          //   // .attr("font-family", "sans-serif")
-          //   // .attr("font-size", "25px")
-          //   // .attr("align","center")
-          //   .text (d.artiest + ":" + d.nummer);
-            creatLine("path",d.nummer,d.artiest,WIDTHBLOCK,HEIGHTBLOCK,AFSTANDTUSSENBLOCK)
-            creatLine("path",d.nummer,d.artiest,breedteblokje,hoogteblockje,afstandtussenblokje)
+            creatLine("path",d.nummer,d.artiest,WIDTHBLOCK,HEIGHTBLOCK,AFSTANDTUSSENBLOCK,"black")
+            creatLine("path",d.nummer,d.artiest,breedteblokje,hoogteblockje,afstandtussenblokje,"black")
 
         })
         .on("click", function(d) {
-          creatLine("clickLine",d.nummer,d.artiest,WIDTHBLOCK,HEIGHTBLOCK,AFSTANDTUSSENBLOCK)
-          creatLine("clickLine",d.nummer,d.artiest,breedteblokje,hoogteblockje,afstandtussenblokje)
+          //kleurklik = colors[Math.floor((Math.random() * 7) )]
+          kleurklik = d3.rgb(Math.floor((Math.random() * 245) +10),Math.floor((Math.random() * 245)+10 ),Math.floor((Math.random() * 245)+10 ))
+
+          creatLine("clickLine",d.nummer,d.artiest,WIDTHBLOCK,HEIGHTBLOCK,AFSTANDTUSSENBLOCK,kleurklik)
+          creatLine("clickLine",d.nummer,d.artiest,breedteblokje,hoogteblockje,afstandtussenblokje,kleurklik)
           d3.select("body").append("div").attr("class","clickShow").style("position","fixed").style("top", "150px").style("left", "650px")
-          d3.select(".clickShow").html(d.artiest + ":" + d.nummer);
+          d3.select(".clickShow").html(d.artiest + ":" + d.nummer)
+          d3.select(".tabel").append("li").text(d.artiest + ":" + d.nummer).style("color", kleurklik)
          })
         .on("mouseout", function(d) {
             svg.selectAll(".hover").remove();
@@ -154,9 +155,9 @@ window.onload = function() {
           .on("dblclick", function(d) {
             document.getElementById('audioElement').play()
            })
-function creatLine(path,nummer,artiest,breedteBlok,hoogteBlok,afstandTussenBlok){
+function creatLine(path,nummer,artiest,breedteBlok,hoogteBlok,afstandTussenBlok,kleur){
+
   lineData = []
-  console.log(breedteBlok)
   for( var u= 0; u<DATADICT.length; u++){
     if (DATADICT[u].nummer == nummer && DATADICT[u].artiest == artiest){
   for (var i = 0; i < 17; i++) {
@@ -168,59 +169,41 @@ function creatLine(path,nummer,artiest,breedteBlok,hoogteBlok,afstandTussenBlok)
       x: ((i+ 1) * (breedteBlok+afstandTussenBlok) + breedteBlok),
       y: ((DATADICT[u].posities[i]) * (hoogteBlok+afstandTussenBlok) + hoogteBlok)
     })
-    tekenLine(path,lineData,breedteBlok)
+    tekenLine(path,lineData,breedteBlok,kleur)
       }
     if (isNumeric(DATADICT[u].posities[i+1]) && i+1 < 17){
     lineData.push({
       x: ((i+1) * (breedteBlok+afstandTussenBlok) + breedteBlok +afstandTussenBlok),
       y:  ((DATADICT[u].posities[i+1]) * (hoogteBlok+afstandTussenBlok) + hoogteBlok)
     })
-    tekenLine(path,lineData,breedteBlok)
+    tekenLine(path,lineData,breedteBlok,kleur)
   }
 }
 }
 }
 }
-  function tekenLine(path,lineData,breedteBlok){
+  function tekenLine(path,lineData,breedteBlok,kleur){
   var line = d3.svg.line()
       .x(function(d){return d.x;})
       .y(function(d){return d.y;})
       .interpolate("linear");
-      console.log(breedteBlok)
  if (breedteBlok > 15){ // error
   svg.append("path")
       .attr("d", function(d) { return line(lineData)})
       .attr("transform", "translate(0,0)")
       .classed(path, true)
       .style("stroke-width", 2)
-          .style("stroke", "black")
+          .style("stroke", kleur)
           .style("fill", "none");
-  // svg.append("text")
-  //         // .attr("y", "50")
-  //         // .attr("x", "80")
-  //         .classed(path, true)
-  //         // .attr("font-family", "sans-serif")
-  //         // .attr("font-size", "48px")
-  //         // .style("fill", "red")
-  //         // aanpassen!!
         }
   else{
-    console.log(lineData)
     svgslider.append("path")
         .attr("d", function(d) { return line(lineData)})
         .attr("transform", "translate(0,0)")
         .classed(path, true)
         .style("stroke-width", 1)
-            .style("stroke", "black")
+            .style("stroke", kleur)
             .style("fill", "none");
-    // svgslider.append("text")
-    //         // .attr("y", "50")
-    //         // .attr("x", "80")
-    //         .classed(path, true)
-    //         // .attr("font-family", "sans-serif")
-    //         // .attr("font-size", "48px")
-    //         // .style("fill", "red")
-    //         // aanpassen!!
   }
 
 }
@@ -255,8 +238,45 @@ function minimap(){
    .attr('fill', function(d){return kleurBlock(d.nummer,d.artiest,d.positie,d.jaartal)})
    .attr('class', 'rectLabel')
 
-   //.text(function(d) { return d.nummer })
+// zoek functie
+   var substringMatcher = function(strs) {
+     return function findMatches(q, cb) {
+       var matches, substringRegex;
 
+       // an array that will be populated with substring matches
+       matches = [];
+
+       // regex used to determine if a string contains the substring `q`
+       substrRegex = new RegExp(q, 'i');
+
+       // iterate through the pool of strings and for any string that
+       // contains the substring `q`, add it to the `matches` array
+       $.each(strs, function(i, str) {
+         if (substrRegex.test(str)) {
+           matches.push(str);
+         }
+       });
+
+       cb(matches);
+     };
+   };
+
+   $('#the-basics .typeahead').typeahead({
+     hint: true,
+     highlight: true,
+     minLength: 1
+   },
+   {
+     name: 'nummers',
+     source: substringMatcher(nummers)
+   });
+   $('input.typeahead').on('typeahead:selected', function(event, selection) {
+     data = selection.split(":")
+       kleurklik = colors[Math.floor((Math.random() * 7) )]
+     creatLine("clickLine",data[1],data[0],WIDTHBLOCK,HEIGHTBLOCK,AFSTANDTUSSENBLOCK,kleurklik)
+     creatLine("clickLine",data[1],data[0],breedteblokje,hoogteblockje,afstandtussenblokje,kleurklik)
+     d3.select(".tabel").append("li").text(data[1] + ":" + data[0]).style("color", kleurklik)
+});
 }
   }
 
